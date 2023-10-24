bb9e07e0f3ed878eb5a5e590d1228e53b7d55be2
Ryon <zhangruiyang36@gmail.com>
2023-10-24 01:13:54 -0400

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import 'semantic-ui-css/semantic.min.css';
 import '@/app/globals.css';
 import type { Metadata } from 'next';
 import { headers } from 'next/headers';
-import TopSearchRibbon from '@/app/(support)/TopSearchRibbon';
 import React from 'react';
-import Menu from '@/app/components/Menu';
 
 export const metadata: Metadata = {
   title: 'Finance App',
